e6890ff694915e6243c30b20f67e24639541cc99
pbhagyashree31 <84366146+pbhagyashree31@users.noreply.github.com>
2021-06-01 10:59:18 +0530

    Add files via upload

---
+++ JUnit/Activities/Calculator.java
@@ -0,0 +1,17 @@
+package Programs;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
